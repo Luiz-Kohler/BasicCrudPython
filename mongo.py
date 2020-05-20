2f53312ece343db2eb11a0ae10fa41e955dd9937
Luiz Felipe dos Santos Kohler <luiz.kohler@hbsis.com.br>
2020-05-19 23:16:23 -0300

    first commit

---
+++ mongo.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'Cluster0'
+app.config['MONGO_URI'] = 'mongodb+srv://kohler:123@cluster0-qxlqo.mongodb.net/test?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+@app.route('/coutries', methods=['GET'])
+def get_all_frameworks():
+    country = mongo.db.country 
+
+    output = []
+
+    for c in country.find():
+        output.append({'name' : c['name']})
+
+    return jsonify({'response' : output})
+
+@app.route('/coutries/<name>', methods=['GET'])
+def get_one_framework(name):
+    country = mongo.db.country
+
+    c = country.find_one({'name' : name})
+
+    if c:
+        output = {'name' : c['name']}
+    else:
+        output = 'No results found'
+
+    return jsonify({'response' : output})
+
+@app.route('/coutries', methods=['POST'])
+def add_framework():
+    framework = mongo.db.framework 
+
+    name = request.json['name']
+
+    country_id = framework.insert({'name' : name})
+    new_country = framework.find_one({'_id' : country_id})
+
+    output = {'name' : new_country['name']}
+
+    return jsonify({'response' : output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
